5e4aec0a5c96a6517ec6a74b8996362159b1c8c8
Jonas Bucher <j.bucher.mn@gmail.com>
2023-06-03 15:31:00 +0200

    Initial setup nixos module

--- flake.nix
+++ flake.nix
@@ -8,98 +8,186 @@
   };
 
   outputs = { self, flake-utils, nixpkgs }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (self: super: rec { })
-          ];
-        };
-      in
-      {
-        packages.guacamole-server = with pkgs;
-          stdenv.mkDerivation
-            rec {
-              pname = "guacamole";
-              version = "1.5.0";
+    flake-utils.lib.eachDefaultSystem
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (self: super: rec { })
+            ];
+          };
+        in
+        {
+          packages.guacamole-server = with pkgs;
+            stdenv.mkDerivation
+              rec {
+                pname = "guacamole";
+                version = "1.5.0";
 
-              src = fetchFromGitHub {
-                owner = "apache";
-                repo = "guacamole-server";
-                rev = version;
-                sha256 = "sha256-aG2Fcll0TgLokI2iAu5Gy7LKGr3FSX2tEFAEFiJCVkw=";
+                src = fetchFromGitHub {
+                  owner = "apache";
+                  repo = "guacamole-server";
+                  rev = version;
+                  sha256 = "sha256-aG2Fcll0TgLokI2iAu5Gy7LKGr3FSX2tEFAEFiJCVkw=";
+                };
+
+                NIX_CFLAGS_COMPILE = [
+                  "-Wno-error=format-truncation"
+                  "-Wno-error=format-overflow"
+                ];
+
+                buildInputs = [
+                  freerdp
+                  autoreconfHook
+                  pkg-config
+                  cairo
+                  libpng
+                  libjpeg_turbo
+                  libossp_uuid
+                  pango
+                  libssh2
+                  libvncserver
+                  libpulseaudio
+                  openssl
+                  libvorbis
+                  libwebp
+                  libtelnet
+                  perl
+                  makeWrapper
+                ];
+
+                propogatedBuildInputs = [
+                  freerdp
+                  autoreconfHook
+                  pkg-config
+                  cairo
+                  libpng
+                  libjpeg_turbo
+                  libossp_uuid
+                  freerdp
+                  pango
+                  libssh2
+                  libvncserver
+                  libpulseaudio
+                  openssl
+                  libvorbis
+                  libwebp
+                  inetutils
+                ];
+
+                patchPhase = ''
+                  patchShebangs ./src/protocols/rdp/**/*.pl
+                  substituteInPlace ./configure.ac --replace "FREERDP2_PLUGIN_DIR=" "FREERDP2_PLUGIN_DIR=${placeholder "out"}/lib"
+
+                '';
+
+                postInstall = ''
+                  wrapProgram $out/sbin/guacd --prefix LD_LIBRARY_PATH ":" $out/lib
+                '';
+
+                meta = with lib; {
+                  description = "Clientless remote desktop gateway";
+                  homepage = "https://guacamole.incubator.apache.org/";
+                  license = licenses.asl20;
+                  maintainers = with maintainers; [ ];
+                  platforms = [ "x86_64-linux" "i686-linux" ];
+                };
               };
 
-              NIX_CFLAGS_COMPILE = [
-                "-Wno-error=format-truncation"
-                "-Wno-error=format-overflow"
-              ];
+          packages.guacamole-client = with pkgs;
+            stdenv.mkDerivation
+              rec {
+                pname = "guacamole-client";
+                version = "1.5.0";
 
-              buildInputs = [
-                freerdp
-                autoreconfHook
-                pkg-config
-                cairo
-                libpng
-                libjpeg_turbo
-                libossp_uuid
-                pango
-                libssh2
-                libvncserver
-                libpulseaudio
-                openssl
-                libvorbis
-                libwebp
-                libtelnet
-                perl
-                makeWrapper
-              ];
+                src = fetchurl {
+                  url = "https://archive.apache.org/dist/guacamole/1.5.0/binary/guacamole-1.5.0.war";
+                  sha256 = "sha256-nE6AeqcLwTjztsxmzES3XaqUSgFZRLmrb3gNeGhJhiA=";
+                };
 
-              propogatedBuildInputs = [
-                freerdp
-                autoreconfHook
-                pkg-config
-                cairo
-                libpng
-                libjpeg_turbo
-                libossp_uuid
-                freerdp
-                pango
-                libssh2
-                libvncserver
-                libpulseaudio
-                openssl
-                libvorbis
-                libwebp
-                inetutils
-              ];
+                dontUnpack = true;
 
-              patchPhase = ''
-                patchShebangs ./src/protocols/rdp/**/*.pl
-                substituteInPlace ./configure.ac --replace "FREERDP2_PLUGIN_DIR=" "FREERDP2_PLUGIN_DIR=${placeholder "out"}/lib"
+                buildInputs = [
+                ];
 
-              '';
+                propogatedBuildInputs = [
+                ];
 
-              postInstall = ''
-                wrapProgram $out/sbin/guacd --prefix LD_LIBRARY_PATH ":" $out/lib
-              '';
+                buildPhase = ''
+                '';
 
-              meta = with lib; {
-                description = "Clientless remote desktop gateway";
-                homepage = "https://guacamole.incubator.apache.org/";
-                license = licenses.asl20;
-                maintainers = with maintainers; [ tomberek ];
-                platforms = [ "x86_64-linux" "i686-linux" ];
+                installPhase = ''
+                  mkdir $out
+                  cp $src $out/${pname}-${version}.war
+                '';
+
+                meta = with lib;
+                  {
+                    description = "Clientless remote desktop gateway";
+                    homepage = "https://guacamole.incubator.apache.org/";
+                    license = licenses.asl20;
+                    maintainers = with maintainers; [ ];
+                    platforms = [ "x86_64-linux" "i686-linux" ];
+                  };
               };
+
+          nixosModules = {
+            guacamole = { config, lib, pkgs, ... }:
+              with lib;
+              let
+                cfg = config.services.guacamole;
+              in
+              {
+                meta = {
+                  maintainers = with maintainers; [ ];
+                };
+
+                ###### interface
+
+                options = {
+
+                  services.guacamole = {
+                    enable = mkEnableOption (lib.mdDoc "Apache Guacamole");
+
+                    extraEnvironment = mkOption {
+                      type = types.listOf types.str;
+                      default = [ ];
+                      example = [ "ENVIRONMENT=production" ];
+                      description = lib.mdDoc "Environment Variables to pass to guacd";
+                    };
+
+                  };
+
+                };
+
+                ###### implementation
+
+                config = mkIf cfg.enable {
+
+                  systemd.services.guacd = {
+                    description = "Apache Guacamole server";
+                    wantedBy = [ "multi-user.target" ];
+                    after = [ "network.target" ];
+
+                    serviceConfig = {
+                      Environment = [
+                        "GUACAMOLE_HOME=/"
+                      ] ++ cfg.extraEnvironment;
+                      ExecStart = "${pkgs.guacamole-server}/bin/guacd -f";
+                    };
+                  };
+                };
+              };
+          };
+
+          devShell =
+            pkgs.mkShell {
+              buildInputs = [ ];
             };
 
-        devShell =
-          pkgs.mkShell {
-            buildInputs = [ ];
-          };
-      }
-    );
+        }
+      );
 
 }
